6e07d8f18bb11c50d11d9e2e2ed60175584cc20d
shimu <shimu@kooteam.com>
2025-10-23 13:16:43 +0800

    更新配置

--- vue/webpack.config.js
+++ vue/webpack.config.js
@@ -1,8 +1,14 @@
-const path = require("path");
-const ZenPlugin = require("zen-app-plugin");
+const path = require('path')
+const ZenPlugin = require('zen-app-plugin')
 
 module.exports = (env, argv) => {
-    const target = env && env.target ? env.target : "";
-    let mode = argv.mode;
-    return ZenPlugin({ target, mode, ROOT: path.resolve("./") });
-};
+  const target = env && env.target ? env.target : ''
+  let mode = argv.mode
+  return ZenPlugin({
+    target,
+    mode,
+    name: 'kooteam',
+    port: '7053',
+    ROOT: path.resolve('./'),
+  })
+}
